9347edb81a9174b27fb32b92fe714876afb8a281
sumaiyaislam69 <si5670673@gmail.com>
2024-11-07 16:28:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
+import Header from './components/Header';
+import Banner from './images/banner.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header></Header>
+ 
+
+      <img src={Banner} alt=''/>
+      <h1>Hello react app</h1>
+
+
+
+      <Button variant="warning">Warning</Button>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <p>Component</p>
+        </div>
+    );
+};
+
+export default header;
